5396ea6f7034bd50cdc8bebc0e44791559a74287
segerpeter07 <segerpeter07@gmail.com>
2017-10-25 21:30:39 -0400

    Got working with motors moving based off serial messages

--- MotorTesting/MotorTesting.ino
+++ MotorTesting/MotorTesting.ino
@@ -8,8 +8,8 @@ Adafruit_DCMotor *M2 = AFMS.getMotor(2);
 Adafruit_DCMotor *M3 = AFMS.getMotor(3);
 Adafruit_DCMotor *M4 = AFMS.getMotor(4);
 
-int SPEED = 20;
-int setPoint = 0;
+int SPEED = 50;
+int msg;
 
 void setup() {
   // put your setup code here, to run once:
@@ -23,42 +23,46 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  delay(1000);
-  myMotor->run(FORWARD);
-  delay(1000);
-  myMotor->run(BACKWARD);
-
   // Read Serial Data
+  int setPoint = 0;
   if(Serial.available() > 0) {
     msg = Serial.read();
+    Serial.println(msg);
     setPoint = int(msg);
   }
 
-  moveBall(setPoint)
+  moveBall(setPoint);
 }
 
 void moveBall(int setPoint) {
   if(setPoint == 1) {
+    M1->setSpeed(SPEED);
     M1->run(FORWARD);
     delay(500);
     M1->run(BACKWARD);
     delay(500);
-  } elif(setPoint == 2) {
+    M1->setSpeed(0);
+  } else if(setPoint == 2) {
+    M2->setSpeed(SPEED);
     M2->run(FORWARD);
     delay(500);
     M2->run(BACKWARD);
     delay(500);
-  } elif(setPoint == 3) {
+    M2->setSpeed(0);
+  } else if(setPoint == 3) {
+    M3->setSpeed(SPEED);
     M3->run(FORWARD);
     delay(500);
     M3->run(BACKWARD);
     delay(500);
-  } elif(setPoint == 4) {
+    M3->setSpeed(0);
+  } else if(setPoint == 4) {
+    M4->setSpeed(SPEED-30);
     M4->run(FORWARD);
     delay(500);
     M4->run(BACKWARD);
     delay(500);
+    M4->setSpeed(0);
   }
 }
 

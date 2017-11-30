66b74b41741dae2203663fb7cdd676356e782f4c
segerpeter07 <segerpeter07@gmail.com>
2017-11-29 20:54:10 -0500

    Modified firmware to support new architecture

--- Production/Backup/Backup.ino
+++ Production/Backup/Backup.ino
@@ -1,13 +1,45 @@
 #include <Wire.h>
+#include <Servo.h>
+
 int x = 0;
 byte data;
 int val;
+int motion_time = 6;  // Time for each motion (s)
+int clock_speed = 16000000; // Arduino clock speed (16 MHz)
+int decrement = 1;    // Amount decreased each cycle
+
+// Servos
+Servo Servo1;
+Servo Servo2;
+Servo Servo3;
+Servo Servo4;
+Servo Servo5;
+Servo Servo6;
+
+
+// Servo counter
+int s1 = 0;
+int s2 = 0;
+int s3 = 0;
+int s4 = 0;
+int s5 = 0;
+int s6 = 0;
+
 
 void setup() {
   // Start the I2C Bus as Master
   Wire.begin();
   Serial.begin(9600);
   pinMode(8, OUTPUT);
+
+  // Attach Servos
+  Servo1.attach(1);     // MIGHT NEED TO CHANGE PINS
+  Servo2.attach(2);
+  Servo3.attach(3);
+  Servo4.attach(4);
+  Servo5.attach(5);
+  Servo6.attach(6);
+
 }
 void loop() {
   Wire.beginTransmission(9); // transmit to device #9
@@ -22,89 +54,66 @@ void loop() {
 
   x = val;  
 
-  // Determine which servo to drive
+  // Start servo drive based off incoming message
   if (x == 1) {
-    for (pos = 0; pos <= 100; pos += 1) { // goes from 0 degrees to 180 degrees
-      // in steps of 1 degree
-      S1.write(pos);              // tell servo to go to position in variable 'pos'
-      delay(15);                       // waits 15ms for the servo to reach the position
-    }
-    for (pos = 100; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-      S1.write(pos);              // tell servo to go to position in variable 'pos'
-      delay(15);                       // waits 15ms for the servo to reach the position
-    }
+    s1 = clock_speed*motion_time;
+    Servo1.write(20);             // CHANGE SPEED
   } else if (x == 2) {
-    for (pos = 0; pos <= 100; pos += 1) { // goes from 0 degrees to 180 degrees
-      // in steps of 1 degree
-      S2.write(pos);              // tell servo to go to position in variable 'pos'
-      delay(15);                       // waits 15ms for the servo to reach the position
-    }
-    for (pos = 100; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-      S2.write(pos);              // tell servo to go to position in variable 'pos'
-      delay(15);                       // waits 15ms for the servo to reach the position
-    }
+    s2 = clock_speed*motion_time;
+    Servo1.write(20);             // CHANGE SPEED
   } else if (x == 3) {
-    for (pos = 0; pos <= 100; pos += 1) { // goes from 0 degrees to 180 degrees
-      // in steps of 1 degree
-      S3.write(pos);              // tell servo to go to position in variable 'pos'
-      delay(15);                       // waits 15ms for the servo to reach the position
-    }
-    for (pos = 100; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-      S3.write(pos);              // tell servo to go to position in variable 'pos'
-      delay(15);                       // waits 15ms for the servo to reach the position
-    }
+    s3 = clock_speed*motion_time;
+    Servo1.write(20);             // CHANGE SPEED
   } else if (x == 4) {
-    for (pos = 0; pos <= 100; pos += 1) { // goes from 0 degrees to 180 degrees
-      // in steps of 1 degree
-      S3.write(pos);              // tell servo to go to position in variable 'pos'
-      S2.write(pos);
-      delay(15);                       // waits 15ms for the servo to reach the position
-    }
-    for (pos = 100; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-      S3.write(pos);              // tell servo to go to position in variable 'pos'
-      S2.write(pos);
-      delay(15);                       // waits 15ms for the servo to reach the position
-    }
+    s4 = clock_speed*motion_time;
+    Servo1.write(20);             // CHANGE SPEED
   } else if (x == 5) {
-    for (pos = 0; pos <= 100; pos += 1) { // goes from 0 degrees to 180 degrees
-      // in steps of 1 degree
-      S1.write(pos);              // tell servo to go to position in variable 'pos'
-      S3.write(pos);
-      delay(15);                       // waits 15ms for the servo to reach the position
-    }
-    for (pos = 100; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-      S1.write(pos);              // tell servo to go to position in variable 'pos'
-      S3.write(pos);
-      delay(15);                       // waits 15ms for the servo to reach the position
-    }
+    s5 = clock_speed*motion_time;
+    Servo1.write(20);             // CHANGE SPEED
   } else if (x == 6) {
-    for (pos = 0; pos <= 100; pos += 1) { // goes from 0 degrees to 180 degrees
-      // in steps of 1 degree
-      S1.write(pos);              // tell servo to go to position in variable 'pos'
-      S2.write(pos);
-      delay(15);                       // waits 15ms for the servo to reach the position
-    }
-    for (pos = 100; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-      S1.write(pos);              // tell servo to go to position in variable 'pos'
-      S2.write(pos);
-      delay(15);                       // waits 15ms for the servo to reach the position
-    }
-  } else if (x == 7) {
-    for (pos = 0; pos <= 100; pos += 1) { // goes from 0 degrees to 180 degrees
-      // in steps of 1 degree
-      S1.write(pos);              // tell servo to go to position in variable 'pos'
-      S2.write(pos);
-      S3.write(pos);
-      delay(15);                       // waits 15ms for the servo to reach the position
-    }
-    for (pos = 100; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-      S1.write(pos);              // tell servo to go to position in variable 'pos'
-      S2.write(pos);
-      S3.write(pos);
-      delay(15);                       // waits 15ms for the servo to reach the position
-    }
+    s6 = clock_speed*motion_time;
+    Servo1.write(20);             // CHANGE SPEED
   }
 
-  val = 0;
-  x = 0;
+  val = 0;  // Reset
+  x = 0;    // Reset
+
+  // Keep servos running until out of time
+  if (s1 > 0) {
+    s1 = s1 - decrement;
+  } else {
+    Servo1.write(90)            // CHANGE SPEED
+    s1 = 0;
+  }
+  if (s2 > 0) {
+    s2 = s2 - decrement;
+  } else {
+    Servo1.write(90)            // CHANGE SPEED
+    s2 = 0;
+  }
+  if (s3 > 0) {
+    s3 = s3 - decrement;
+  } else {
+    Servo1.write(90)            // CHANGE SPEED
+    s3 = 0;
+  }
+  if (s4 > 0) {
+    s4 = s4 - decrement;
+  } else {
+    Servo1.write(90)            // CHANGE SPEED
+    s4 = 0;
+  }
+  if (s5 > 0) {
+    s5 = s5 - decrement;
+  } else {
+    Servo1.write(90)            // CHANGE SPEED
+    s5 = 0;
+  }
+  if (s6 > 0) {
+    s6 = s6 - decrement;
+  } else {
+    Servo1.write(90)            // CHANGE SPEED
+    s6 = 0;
+  }
+  
 }

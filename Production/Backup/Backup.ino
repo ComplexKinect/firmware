71439b77e4970c79752dab0ed7013f7b88842deb
segerpeter07 <segerpeter07@gmail.com>
2017-12-05 11:34:03 -0500

    Reworte firmware logic

--- Production/Backup/Backup.ino
+++ Production/Backup/Backup.ino
@@ -1,50 +1,68 @@
 #include <Wire.h>
 #include <Servo.h>
 
+
+/* ----- Globals ----- */
 int x = 0;
 byte data;
 int val;
 int motion_time = 6;  // Time for each motion (s)
-int clock_speed = 25; // Arduino clock speed (16 MHz)
-int decrement = 1;    // Amount decreased each cycle
+int clock_speed = 25; // Arduino clock speed (26 MHz)
+int decrement = 2;    // Amount decreased each cycle
 
 // Servos
 Servo Servo1;
 Servo Servo2;
 Servo Servo3;
+Servo Servo4;
+Servo Servo5;
+Servo Servo6;
+
 
 // Servo Speeds
 int speed1 = 68;      // NEED TO CHANGE
-int speed2 = 104;      // NEED TO CHANGE
+int speed2 = 204;     // NEED TO CHANGE
 int speed3 = 50;      // NEED TO CHANGE
+int speed4 = 50;      // NEED TO CHANGE
+int speed5 = 50;      // NEED TO CHANGE
+int speed6 = 50;      // NEED TO CHANGE
 
 // Servo Stops
-int stop1 = 47.5;       // NEED TO CHANGE
+int stop1 = 47.5;     // NEED TO CHANGE
 int stop2 = 84;       // NEED TO CHANGE
-int stop3 = 0;       // NEED TO CHANGE
+int stop3 = 0;        // NEED TO CHANGE
+int stop4 = 0;        // NEED TO CHANGE
+int stop5 = 0;        // NEED TO CHANGE
+int stop6 = 0;        // NEED TO CHANGE
+
 
 // Servo counter
 int s1 = 0;
 int s2 = 0;
 int s3 = 0;
+int s4 = 0;
+int s5 = 0;
+int s6 = 0;
 
 
 void setup() {
   // Start the I2C Bus as Master
   Wire.begin();
   Serial.begin(9600);
-  pinMode(8, OUTPUT);
+  pinMode(8, OUTPUT);     // Debug LED
 
   // Attach Servos
-  Servo1.attach(4);     // MIGHT NEED TO CHANGE PINS
-  Servo2.attach(5);
-  Servo3.attach(7);
+  //Servo2.attach(2);     // MIGHT NEED TO CHANGE PINS
+  //Servo2.attach(2);
+  //Servo3.attach(3);
 
 }
 void loop() {
+  /* ----- Begin I2C ----- */
   Wire.beginTransmission(9); // transmit to device #9
   Wire.write(x);              // sends x 
   Wire.endTransmission();    // stop transmitting
+  /* ----- End I2C ----- */
 
   // Read Serial Data
   if(Serial.available() > 0) {
@@ -56,66 +74,69 @@ void loop() {
   x = val; 
   Serial.println(x); 
 
-  // Start servo drive based off incoming message
-  if (x == 1) {
+
+  /* ----- Start Servos ----- */
+  // Servo 1
+  if (x % 10 == 1) {
     s1 = clock_speed*motion_time;
-    Serial.println(s1);
-    Servo1.attach(4);
+    Servo1.attach(1);
     Servo1.write(speed1);
-  } else if (x == 2) {
+  }
+
+  // Servo 2
+  if ((x/10) % 10 == 1) {
     s2 = clock_speed*motion_time;
-    Servo2.attach(5);
+    Servo2.attach(2);
     Servo2.write(speed2);
-  } else if (x == 3) {
+  }
+
+  // Servo 3
+  if ((x/100) % 10 == 1) {
     s3 = clock_speed*motion_time;
-    Servo3.attach(7);
+    Servo3.attach(3);
     Servo3.write(speed3);
-  } else if (x == 4) {
-    s2 = clock_speed*motion_time;
-    s3 = clock_speed*motion_time;
-    Servo2.attach(5);
-    Servo3.attach(7);
-    Servo2.write(speed2);
-    Servo3.write(speed3);
-  } else if (x == 5) {
-    s1 = clock_speed*motion_time;
-    s3 = clock_speed*motion_time;
-    Servo1.attach(4);
-    Servo3.attach(7);
-    Servo1.write(speed1);
-    Servo3.write(speed3);
-  } else if (x == 6) {
-    s1 = clock_speed*motion_time;
-    s2 = clock_speed*motion_time;
-    Servo1.attach(4);
-    Servo2.attach(5);
-    Servo1.write(speed1);
-    Servo2.write(speed2);
-  } else if (x == 7) {
-    s1 = clock_speed*motion_time;
-    s2 = clock_speed*motion_time;
-    s3 = clock_speed*motion_time;
-    Servo1.attach(4);
-    Servo2.attach(5);
-    Servo3.attach(7);
-    Servo1.write(speed1);
-    Servo2.write(speed2);
-    Servo3.write(speed3);
+  }
+
+  // Servo 4
+  if ((x/1000) % 10 == 1) {
+    s4 = clock_speed*motion_time;
+    Servo4.attach(4);
+    Servo4.write(speed4);
+  }
+
+  // Servo 5
+  if ((x/10000) % 10 == 1) {
+    s5 = clock_speed*motion_time;
+    Servo5.attach(5);
+    Servo5.write(speed5);
+  }
+
+  // Servo 6
+  if ((x/100000) % 10 == 1) {
+    s6 = clock_speed*motion_time;
+    Servo6.attach(6);
+    Servo6.write(speed6);
   }
 
   val = 0;  // Reset
   x = 0;    // Reset
+  /* ----- End Servo Starting ----- */
 
-  // Keep servos running until out of time
+
+
+  /* ----- Start Time Keeping ----- */
+  // Servo 1
   if (s1 > 0) {
     Serial.print("Counter: ");
     Serial.println(s1);
     s1 = s1 - decrement;
   } else {
-    //Servo1.write(stop1);
+    //Servo1.write(stop1);      // May need to be commented out... -->
     Servo1.detach();
     s1 = 0;
   }
+
+  // Servo 2
   if (s2 > 0) {
     s2 = s2 - decrement;
   } else {
@@ -123,6 +144,8 @@ void loop() {
     Servo2.detach();
     s2 = 0;
   }
+
+  // Servo 3
   if (s3 > 0) {
     s3 = s3 - decrement;
   } else {
@@ -130,5 +153,33 @@ void loop() {
     Servo3.detach();
     s3 = 0;
   }
+
+  // Servo 4
+  if (s4 > 0) {
+    s4 = s4 - decrement;
+  } else {
+    //Servo4.write(stop4);      // May need to be commented out... -->
+    Servo4.detach();
+    s4 = 0;
+  }
+
+  // Servo 5
+  if (s5 > 0) {
+    s5 = s5 - decrement;
+  } else {
+    //Servo4.write(stop4);      // May need to be commented out... -->
+    Servo5.detach();
+    s5 = 0;
+  }
+
+  // Servo 6
+  if (s6 > 0) {
+    s6 = s6 - decrement;
+  } else {
+    //Servo4.write(stop4);      // May need to be commented out... -->
+    Servo6.detach();
+    s6 = 0;
+  }
+  /* ----- End time tracking ----- */
   
 }
--- TESTER/TESTER.ino
+++ TESTER/TESTER.ino
@@ -1,12 +1,24 @@
+#include <Servo.h>
+
+Servo Servo1;
+Servo Servo2;
+Servo Servo3;
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+
+  //Servo1.attach(1);
+  //Servo2.attach(2);
+  //Servo3.attach(3);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(7, HIGH);
-  delay(5000);
-  digitalWrite(7, LOW);
+  Servo1.attach(2);
+  Servo1.write(68);
+  delay(500);
+  Servo1.write(47.5);
+  Servo1.detach();
   delay(500);
 }
